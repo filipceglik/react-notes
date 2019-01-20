6f13f3b6dc36dabbe04f159b9036e2aa3c0c2a8f
Filip Ceglik <fceglik@gmail.com>
2019-01-20 18:46:18 +0100

    adding and removing notes

--- src/Board.js
+++ src/Board.js
@@ -1,32 +1,49 @@
 import React, {Component} from 'react'
 import Note from './Note'
+import {FaPlus} from 'react-icons/fa'
 
 class Board extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      notes: [
-        {
-          id: 0,
-          note: "Call Lisa"
-        },
-        {
-          id:1,
-          note: "Email John"
-        }
-      ]
+      notes: []
     }
     this.eachNote = this.eachNote.bind(this)
     this.update = this.update.bind(this)
+    this.remove = this.remove.bind(this)
+    this.add = this.add.bind(this)
+    this.nextId = this.nextId.bind(this)
   }
   eachNote(note, i) {
     return (
       <Note key={i}
         index={i}
-        onChange={this.update}>
+        onChange={this.update}
+        onRemove={this.remove}>
         {note.note}
         </Note>
     )
+  }
+  add(text) {
+    this.setState(prevState => ({
+      notes: [
+        ...prevState.notes,
+        {
+          id: this.nextId(),
+          note:text
+        }
+      ]
+    }))
+  }
+  nextId() {
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+  }
+  remove(id) {
+    console.log('removing note', id)
+    this.setState(prevState => ({
+      notes:prevState.notes.filter(note => note.id !== id)
+    }))
   }
   update(newText, i) {
     console.log('updating item', i, newText)
@@ -40,6 +57,9 @@ class Board extends Component {
     return (
       <div className="board">
         {this.state.notes.map(this.eachNote)}
+        <button onClick={this.add.bind(null, "New note")}
+          id="add"><FaPlus/>
+        </button>
       </div>
     )
   }
